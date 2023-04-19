d331b575716719d785b703862a80591044be7e3e
LAPTOP-3RHO7SBV\Asus <lehonganh1903@gmail.com>
2023-04-19 14:45:35 +0700

    update add dish

--- res/views.py
+++ res/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.core.files.storage import FileSystemStorage
 from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import login_required
 
-# from .forms import DishForm
 from .models import *
 
 
@@ -14,19 +15,51 @@ def dashboard(request):
 # DISH
 
 
-# def add_dish(request):
-#     if request.method == 'POST':
-#         form = DishForm(request.POST)
-#         if form.is_valid():
-#             form.save()
+def add_dish(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        status = request.POST['status']
+        price = request.POST['price']
+        menu_value = request.POST.get('menu')
+        menu_instance = Menu.objects.get(type=menu_value)
+        image = request.FILES['image']
+        fs = FileSystemStorage()
+        filename = fs.save(image.name, image)
+        if (name == "")  or (status is None) or (menu_instance is None) or (price == "") :
+            dishs = Dish.objects.filter()
+            error_msg = "Please enter valid details"
+            return render(request, 'res : dish_list', {'dishs': dishs, 'error_msg': error_msg})
 
-#             return redirect('/admin/dish_list') # Điều hướng sau khi thêm món ăn thành công
-#     else:
-#         form = DishForm()
-#     return render(request, 'admin/add_dish.html', {'form': form})
+        dish = Dish.objects.create(name=name,  status=status, menu=menu_instance,
+                                   price=price, image=filename)
+        dish.save()
+        dishs = Dish.objects.filter()
+    return redirect(reverse('res:dish_list'))
+
 def dish_list(request): 
     dishes = Dish.objects.all()
     return render(request, 'admin/dish.html', {'dishes': dishes})
+
+@login_required
+@staff_member_required
+def edit_food(request, dishID):
+    dish = Dish.objects.filter(id=dishID)[0]
+    if request.method == "POST":
+        if request.POST['price'] != "":
+            dish.price = request.POST['price']
+
+        if request.POST['discount'] != "":
+            dish.discount = request.POST['discount']
+
+        status = request.POST.get('disabled')
+        if status == 'on':
+            dish.status = "Disabled"
+        else:
+            dish.status = "Enabled"
+
+        dish.save()
+    return redirect('hotel:dishs_admin')
+
 
 # CUSTOMER
 
