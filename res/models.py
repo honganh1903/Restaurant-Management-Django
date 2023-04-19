69717ff02a6bdc16086a816a4fa89a5ad44fcaa4
bongmaxuyentuong1 <bongmaxuyentuong1@gmail.com>
2023-04-19 11:14:02 +0700

    new version

--- res/models.py
+++ res/models.py
@@ -7,17 +7,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Student(models.Model):
-    roll = models.CharField(max_length=100)
-    name = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-    address = models.CharField(max_length=200)
-    phone = models.CharField(max_length=10)
-
-    def __str__(self):
-        return self.name
-
-
 class Customer(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     address = models.TextField()
