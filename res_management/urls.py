02226fcefc2017ac32541ccb9968ac10f174de9c
bongmaxuyentuong1 <bongmaxuyentuong1@gmail.com>
2023-04-19 11:16:45 +0700

    update urls

--- res_management/urls.py
+++ res_management/urls.py
@@ -2,10 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.urls import re_path
 
-from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
-from res import views
 urlpatterns = [
     re_path(r'', include(('res.urls', 'res'), namespace='res')),
     re_path(r'^res/', include('res.urls')),
