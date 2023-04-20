0c0b26fc4568c68d87fb7319c84b7f666e274f41
LAPTOP-3RHO7SBV\Asus <lehonganh1903@gmail.com>
2023-04-20 10:32:39 +0700

    update add dish 20/4

--- res/urls.py
+++ res/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 urlpatterns = [
     re_path(r'^dashboard$',dashboard, name='dashboard'),
     re_path(r'^dashboard/dish_list/$',dish_list, name='dish_list'),
+    re_path(r'^dashboard/add_dish/$',add_dish, name='add_dish'),
     re_path(r'^dashboard/customer_list/$',customer_list, name='customer_list'),
     re_path(r'^dashboard/employee_list$',employee_list, name='employee_list'),
     re_path(r'^dashboard/order_list$',order_list, name='order_list'),
